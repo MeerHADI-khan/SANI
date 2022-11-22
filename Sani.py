99823ab7922e954dc984782ae926dc96518eb24d
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-22 13:04:25 +0500

    Sani.py
    pkg update
     pkg upgrade
     pkg install python
    pkg install python2
     pip install requests
     pip install mechanize
     pip install lolcat
     pip install rich
     pip install BeautifulSoup
     pkg install git
    git clone
    https://github.com/MeerHADI-khan/SANI.git
    Cd SANI
    python Sani.py

---
+++ Sani.py
@@ -0,0 +1,76 @@
+<p align="center">
+<a href="https://MMEERHAD-KHAN.github.io/"><img title="Made in PK" src="https://img.shields.io/badge/MADE%20IN-PK-SCRIPT?colorA=%23ff8100&colorB=%23017e40&colorC=%23ff0000&style=for-the-badge"></a>
+
+<a href="https://MMEERHAD-KHAN.github.io/"><img title="Version" src="https://img.shields.io/badge/Version-1.0.5-green.svg?style=flat-square"></a>
+
+<h3 align="center">
+  <img src="https://emoji.discord.st/emojis/768b108d-274f-4f44-a634-8477b16efce7.gif" width="25">
+  &nbsp;                                                                                                                                             
+                                                                                                                                             
+MMMMMMMM               MMMMMMMMRRRRRRRRRRRRRRRRR        HHHHHHHHH     HHHHHHHHH               AAA               DDDDDDDDDDDDD      IIIIIIIIII
+M:::::::M             M:::::::MR::::::::::::::::R       H:::::::H     H:::::::H              A:::A              D::::::::::::DDD   I::::::::I
+M::::::::M           M::::::::MR::::::RRRRRR:::::R      H:::::::H     H:::::::H             A:::::A             D:::::::::::::::DD I::::::::I
+M:::::::::M         M:::::::::MRR:::::R     R:::::R     HH::::::H     H::::::HH            A:::::::A            DDD:::::DDDDD:::::DII::::::II
+M::::::::::M       M::::::::::M  R::::R     R:::::R       H:::::H     H:::::H             A:::::::::A             D:::::D    D:::::D I::::I  
+M:::::::::::M     M:::::::::::M  R::::R     R:::::R       H:::::H     H:::::H            A:::::A:::::A            D:::::D     D:::::DI::::I  
+M:::::::M::::M   M::::M:::::::M  R::::RRRRRR:::::R        H::::::HHHHH::::::H           A:::::A A:::::A           D:::::D     D:::::DI::::I  
+M::::::M M::::M M::::M M::::::M  R:::::::::::::RR         H:::::::::::::::::H          A:::::A   A:::::A          D:::::D     D:::::DI::::I  
+M::::::M  M::::M::::M  M::::::M  R::::RRRRRR:::::R        H:::::::::::::::::H         A:::::A     A:::::A         D:::::D     D:::::DI::::I  
+M::::::M   M:::::::M   M::::::M  R::::R     R:::::R       H::::::HHHHH::::::H        A:::::AAAAAAAAA:::::A        D:::::D     D:::::DI::::I  
+M::::::M    M:::::M    M::::::M  R::::R     R:::::R       H:::::H     H:::::H       A:::::::::::::::::::::A       D:::::D     D:::::DI::::I  
+M::::::M     MMMMM     M::::::M  R::::R     R:::::R       H:::::H     H:::::H      A:::::AAAAAAAAAAAAA:::::A      D:::::D    D:::::D I::::I  
+M::::::M               M::::::MRR:::::R     R:::::R     HH::::::H     H::::::HH   A:::::A             A:::::A   DDD:::::DDDDD:::::DII::::::II
+M::::::M               M::::::MR::::::R     R:::::R     H:::::::H     H:::::::H  A:::::A               A:::::A  D:::::::::::::::DD I::::::::I
+M::::::M               M::::::MR::::::R     R:::::R     H:::::::H     H:::::::H A:::::A                 A:::::A D::::::::::::DDD   I::::::::I
+MMMMMMMM               MMMMMMMMRRRRRRRR     RRRRRRR     HHHHHHHHH     HHHHHHHHHAAAAAAA                   AAAAAAADDDDDDDDDDDDD      IIIIIIIIII
+                                                                                                                                             
+                                                                                                                                             
+                                                                                                                                             
+                                                                                                                                             
+                                                                                                                                             
+                                                                                                                                             
+                                                                                                                                             
+&nbsp;
+  <img src="https://emoji.discord.st/emojis/768b108d-274f-4f44-a634-8477b16efce7.gif" width="25">
+  
+![boysdpz_c4663e3_1656850427534_sc_cmprsd_75](https://user-images.githubusercontent.com/114737357/
+
+</h3>
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-1.svg">
+
+[[![Typing SVG](https://readme-typing-svg.demolab.com?font=Fira+Code&pause=800&color=D4202000&width=435&lines=MR+HADI%F0%9F%91%BF%F0%9F%91%BF%F0%9F%91%BF+naam+to+suna+hoga;Har+Kisi+Ke+bus+Ki+Baat+Nahin+k+MR+HADi+se+baat+kare%F0%9F%91%BF%F0%9F%98%92)](https://git.io/typing-svg)
+
+![106824690-8dd73a00-66ad-11eb-89e2-53e13ac6f594 (1)](https://user-images.githubusercontent.com/79738922/150628863-e161ecb3-06fe-4656-be20-9122ed533309.gif)
+
+![Alt text](https://github.com/MRVIVEK-CODER/MRVIVEK-CODER/raw/main/md7Oqrf.gif)
+
+[<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Fira+Code&pause=800&color=D4202000&width=435&lines=MR+HADI%F0%9F%91%BF%F0%9F%91%BF%F0%9F%91%BF+naam+to+suna+hoga;Har+Kisi+Ke+bus+Ki+Baat+Nahin+k+MR+HADi+se+baat+kare%F0%9F%91%BF%F0%9F%98%92" alt="Typing SVG" /></a>
+
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?Mee&label=Profile%20views&color=0e75b6&style=flat" alt="SANI-KHAN" /> </p>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=MMEERHAD-KHAN" alt="MMEERHAD-KHAN" /></a> </p>
+
+<p align="left"> <a href="https//"facebook target="blank"><img src="https://img.shields.io/facebook/follow/?logo=twitter&style=for-the-badge" alt="" /></a> </p>
+
+<img width="800px" src="https://user-images.githubusercontent.com/116461/76165260-c6c00500-6112-11ea-9cda-0a6cb9b72e8f.gif" />
+
+•Ⓑ• <b>VISTOR COUNT •Ⓑ•
+
+  <img src="https://profile-counter.glitch.me/N1ght420/count.svg" />
+
+- Contact Me **Meershahzeba@gmail.com**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://www.facebook.com"SaMI target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="Baizid.171" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://hive.apache.org/" target="_blank" rel="noreferrer"> <img0 src="https://www.vectorlogo.zone/logos/apache_hive/apache_hive-icon.svg" alt="hive" width="40" height="40"/> </a> <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/> </a> <a href="https://kotlinlang.org" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg" alt="kotlin" width="40" height="40"/> </a> <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=HADi&show_icons=true&locale=en&layout=compact" alt="HADi#C64D88" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=HADi&show_icons=true&locale=en" alt="alifvau" /></p>
+
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=HADi&" alt="HADi" /></p>
